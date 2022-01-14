4564066370d07365a0487e7794ddae82daff60c1
Yaninck DUTHE <yannickduthe@hotmail.com>
2022-01-14 17:51:17 +0100

    refresh vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ const App = () => (
   <Admin
     disableTelemetry
     history={history}
-    title="META"
+    title="Meta"
     authProvider={authProvider}
     dataProvider={dataProvider}
     i18nProvider={i18nProvider}
